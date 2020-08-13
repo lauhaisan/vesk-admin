fab3800e5dc15b3e56a307de2071fd8ad8d5e939
phat nguyen <phat.nguyen@terralogic.com>
2020-08-13 11:13:16 +0700

    fix side menu

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,10 +1,37 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, Fragment } from "react";
 import { NavLink } from "react-router-dom";
 import "./index.scss";
 
 export default class SideMenu extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openUser: false
+    };
+  }
+
+  componentDidMount() {
+    if (window.location.pathname.includes("users")) {
+      this.setState({
+        openUser: true
+      });
+    }
+  }
+
+  handleClickUser = () => {
+    const { openUser } = this.state;
+    this.setState({
+      openUser: !openUser
+    });
+  };
+
   render() {
+    const { openUser } = this.state;
     const classNameMenu = "closeLeftPanel";
+    const iconChevronUser = openUser
+      ? "fas fa-chevron-up"
+      : "fas fa-chevron-down";
+
     return (
       <div className={classNameMenu}>
         <div className={`${classNameMenu}__menu`}>
@@ -17,15 +44,41 @@ export default class SideMenu extends PureComponent {
             <i className="fas fa-home itemMenu__icon"></i>
             <span className="itemMenu__text">Home</span>
           </NavLink>
+          <Fragment>
+            <div className="menu__parent" onClick={this.handleClickUser}>
+              <span>Users</span>
+              <i className={`${iconChevronUser} menu__parent__icon`}></i>
+            </div>
+            {openUser && (
+              <Fragment>
+                <NavLink
+                  activeClassName={`${classNameMenu}__menu__itemMenu--active`}
+                  to="/users"
+                  className={`${classNameMenu}__menu__itemMenu`}
+                >
+                  <i className="fas fa-users itemMenu__icon"></i>
+                  <span className="itemMenu__text">Manage</span>
+                </NavLink>
+                <NavLink
+                  activeClassName={`${classNameMenu}__menu__itemMenu--active`}
+                  to="/users-exchange/"
+                  className={`${classNameMenu}__menu__itemMenu`}
+                >
+                  <i className="fas fa-users itemMenu__icon"></i>
+                  <span className="itemMenu__text">Exchange</span>
+                </NavLink>
+              </Fragment>
+            )}
+          </Fragment>
 
-          <NavLink
+          {/* <NavLink
             activeClassName={`${classNameMenu}__menu__itemMenu--active`}
             to="/users"
             className={`${classNameMenu}__menu__itemMenu`}
           >
             <i className="fas fa-users itemMenu__icon"></i>
             <span className="itemMenu__text">Users</span>
-          </NavLink>
+          </NavLink> */}
 
           <NavLink
             activeClassName={`${classNameMenu}__menu__itemMenu--active`}
