3515dff66f393d707854eff43c144bf948c46c3c
Phat Nguyen <phat.nguyen@terralogic.com>
2020-09-12 20:07:42 +0700

    change API url login, fetch list user, user by id, list media social, media social by id, list ads, fetch ads by id

--- src/constant/url.js
+++ src/constant/url.js
@@ -1,22 +1,22 @@
 const URL = {
   SIGNUP: "/v1/auth/register",
-  SIGNIN: "/v1/auth/login",
+  SIGNIN: "/v1/auth/admin/login",
   LOGOUT: "/user/logout",
   REFRESH_TOKEN: "/user/refresh-token",
   GET_MY_INFO: "/user/get-my-info",
   GET_USER_INFO: "/v1/vesk/users/my-info",
   PRODUCT: "/products",
 
-  GET_LIST_USER: "/v1/vesk/users/search",
-  GET_USER_BY_ID: "/v1/vesk/users/userId",
+  GET_LIST_USER: "/v1/vesk/public/users/search",
+  GET_USER_BY_ID: "/v1/vesk/public/users/userId",
   EDIT_USER: "/v1/vesk/admin/users",
 
-  GET_LIST_ADVERTISING: "/v1/vesk/ads",
+  GET_LIST_ADVERTISING: "/v1/vesk/public/ads",
   ADMIN_CRUD_ADVERTISING: "/v1/vesk/admin/ads",
 
-  GET_LIST_SOCIAL_MEDIA: "/v1/vesk/e-social-media",
+  GET_LIST_SOCIAL_MEDIA: "/v1/vesk/public/e-social-media",
   ADMIN_CRUD_SOCIAL_MEDIA: "/v1/vesk/admin/e-social-media",
-  SOCIAL_MEDIA_SEARCH: "/v1/vesk/e-social-media/search",
+  SOCIAL_MEDIA_SEARCH: "/v1/vesk/public/e-social-media/search",
 
   CREATE_EXCHANGE: "/v1/vesk/admin/exchange",
 };
